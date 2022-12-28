45e6a8d43ebe148a03b0f0529ed33192b5862f2c
Seifbarouni <seif.barouni11@gmail.com>
2022-12-28 15:16:21 +0100

    add employee

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Get the RDS connection details from environment variables
 const host = "seif-rds.cdgxzsc1f3sx.us-east-1.rds.amazonaws.com";
@@ -22,20 +22,35 @@ const connection = mysql.createConnection({
 // Connect to the database
 connection.connect((error) => {
   if (error) throw error;
-  console.log('Connected to the database');
+  console.log("Connected to the database");
 });
 
 // Set up a route to get employee data
-app.get('/employees', (req, res) => {
-  connection.query('SELECT * FROM employees', (error, results) => {
+app.get("/employees", (req, res) => {
+  connection.query("SELECT * FROM employees", (error, results) => {
     if (error) throw error;
 
     // Render the employee data in a template
-    res.render('employees', { employees: results });
+    res.render("employees", { employees: results });
   });
+});
+
+app.post("/employees", (req, res) => {
+  const name = req.body.name;
+  const department = req.body.department;
+
+  connection.query(
+    "INSERT INTO employees (name, department) VALUES (?, ?)",
+    [name, department],
+    (error, results) => {
+      if (error) throw error;
+
+      res.redirect("/employees");
+    }
+  );
 });
 
 // Start the server
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log("Server listening on port 3000");
 });
